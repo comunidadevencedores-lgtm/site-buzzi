71e2f315538363eb41e9e9446acb55fd67122fb1
Victor <comunidadevencedores@gmail.com>
2026-01-09 12:26:34 -0300

    Use custom server to bind port on Render

---
+++ next.js
@@ -0,0 +1,15 @@
+const { createServer } = require("http");
+const next = require("next");
+
+const port = process.env.PORT || 3000;
+const host = "0.0.0.0";
+const app = next({ dev: false, hostname: host, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, host, () => {
+    console.log(`> Ready on http://${host}:${port}`);
+  });
+});
